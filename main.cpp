e4eafc6931fb7e8b9ac1a1097f12427e513c2ff0
re8code <won@re8code.com>
2025-08-09 21:21:13 +0900

    p13

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 #include <typeinfo>
 
 int main () {
-    printf("1) %s, %lu\n", typeid(1).name(),      sizeof(1));
-    printf("2) %s, %lu\n", typeid(2L).name(),     sizeof(2L));     // 소문자 'l' 가능
-    printf("3) %s, %lu\n", typeid(0.123).name(),  sizeof(0.123));
-    printf("4) %s, %lu\n", typeid(0.123F).name(), sizeof(0.123F)); // 소문자 'f' 가능
-    printf("5) %s, %lu\n", typeid('a').name(),    sizeof('a'));
-    printf("6) %s, %lu\n", typeid("a").name(),    sizeof("a"));
-    printf("7) %s, %lu\n", typeid(true).name(),   sizeof(true));
+    auto a1 = 10, a2 = 20;                  // O)
+    auto b1 = 0b00001111, b2 = 0B11110000;  // 1) 02진수 표식자
+    printf("1) b1 = %d[%s], b2 = %d[%s]\n", b1,typeid(b1).name(), b2,typeid(b2).name());
+
+    auto c1 = 0xff00l, c2 = 0X00FFL;        // 2) 16진수 표식자
+    printf("2) c1 = %ld[%s], c2 = %ld[%s]\n", c1,typeid(c1).name(), c2,typeid(c2).name());
+
+    auto d1 = 0123456;                      // 3) 08진수 표식자
+    printf("3) d1 = %d[%s]\n", d1, typeid(d1).name());
+
+    auto e1 = 0.123456f, e2 = 0.456789F;    // 4) float 표식자
+    printf("4) e1 = %.20f[%s], e2 = %.15f[%s]\n", e1,typeid(e1).name(), e2,typeid(e2).name());
+
+    auto f1 = 0.123456;                     // 5)
+    printf("5) f1 = %.20f[%s]", f1, typeid(f1).name());
 }
